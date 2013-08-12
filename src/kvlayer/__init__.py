c2412690e4172387b4eef1d1935751d9792ae10b
John R. Frank <jrf@diffeo.com>
2013-08-12 12:28:55 +0000

    constructing external package interface

---
+++ src/kvlayer/__init__.py
@@ -0,0 +1,9 @@
+'''
+
+This software is released under an MIT/X11 open source license.
+
+Copyright 2012-2013 Diffeo, Inc.
+'''
+
+from kvlayer._client import client
+from kvlayer._exceptions import MissingID, DatabaseEmpty
