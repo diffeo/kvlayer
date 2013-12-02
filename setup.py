2133b4b05a52e671dfbc23560e2b15fbf6af9d72
Steven Bauer <bauer@diffeo.com>
2013-12-02 21:19:58 +0000

    Fix after filter

---
+++ setup.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import fnmatch
+import subprocess
+
+## prepare to run PyTest as a command
+from distutils.core import Command
+
+from setuptools import setup, find_packages
+
+from version import get_git_version
+VERSION, SOURCE_LABEL = get_git_version()
+PROJECT = 'kvlayer'
+URL = 'http://diffeo.com'
+AUTHOR = 'Diffeo, Inc.'
+AUTHOR_EMAIL = 'support@diffeo.com'
+DESC = 'table-oriented abstraction layer over key-value stores'
+
+def read_file(file_name):
+    file_path = os.path.join(
+        os.path.dirname(__file__),
+        file_name
+        )
+    return open(file_path).read()
+
+def recursive_glob(treeroot, pattern):
+    results = []
+    for base, dirs, files in os.walk(treeroot):
+        goodfiles = fnmatch.filter(files, pattern)
+        results.extend(os.path.join(base, f) for f in goodfiles)
+    return results
+
+def recursive_glob_with_tree(treeroot, pattern):
+    results = []
+    for base, dirs, files in os.walk(treeroot):
+        goodfiles = fnmatch.filter(files, pattern)
+        one_dir_results = []
+        for f in goodfiles:
+            one_dir_results.append(os.path.join(base, f))
+        results.append((base, one_dir_results))
+    return results
+
+def _myinstall(pkgspec):
+    setup(
+        script_args = ['-q', 'easy_install', '-v', pkgspec],
+        script_name = 'easy_install'
+    )
+
+class PyTest(Command):
+    '''run py.test'''
+
+    description = 'runs py.test to execute all tests'
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        if self.distribution.install_requires:
+            for ir in self.distribution.install_requires:
+                _myinstall(ir)
+        if self.distribution.tests_require:
+            for ir in self.distribution.tests_require:
+                _myinstall(ir)
+
+        # reload sys.path for any new libraries installed
+        import site
+        site.main()
+        print sys.path
+        # use pytest to run tests
+        pytest = __import__('pytest')
+        ## always run all the tests
+        if pytest.main(['-n', '3', '-s', 'src', '--runslow', '--runperf']):
+            sys.exit(1)
+
+setup(
+    name=PROJECT,
+    version=VERSION,
+    description=DESC,
+    long_description=read_file('README.rst'),
+    author=AUTHOR,
+    license='MIT/X11 license http://opensource.org/licenses/MIT',
+    author_email=AUTHOR_EMAIL,
+    url=URL,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    cmdclass={'test': PyTest},
+    # We can select proper classifiers later
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',  ## MIT/X11 license http://opensource.org/licenses/MIT
+    ],
+    tests_require=[
+        'guppy',
+        'pytest',
+        'pytest-cov',
+        'pytest-xdist',
+        'pytest-timeout',
+        'pytest-incremental',
+        'pytest-capturelog',
+    ],
+    # psycopg2 may be commented out to operate without postgres support
+    install_requires=[
+        # 'psycopg2',
+        'pycassa >= 1.10',
+        'pyaccumulo_dev >= 1.5.0-SNAPSHOT.1',
+        'pyyaml',
+        'cql'
+    ],
+    # include_package_data = True,
+    package_data={
+        # If any package contains *.txt or *.rst files, include them:
+        # '': ['*.txt', '*.rst'],
+        # And include any files found in the 'data' package:
+        # '': recursive_glob('src/data/', '*')
+        '': recursive_glob('data/', '*')
+    },
+)
