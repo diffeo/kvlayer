18078c28e90d5f4e7ca02feeafe5b62d2950e020
David Maze <dmaze@diffeo.com>
2014-02-20 21:48:52 +0000

    Use new yakonfig configuration description

--- kvlayer/_client.py
+++ kvlayer/_client.py
@@ -3,14 +3,17 @@ This software is released under an MIT/X11 open source license.
 
 Copyright 2012-2014 Diffeo, Inc.
 '''
+from __future__ import absolute_import
 import argparse
 from cStringIO import StringIO
 import logging
 import sys
 import termios
 import tty
+
 import yaml
 
+import kvlayer
 from kvlayer._accumulo import AStorage
 from kvlayer._cassandra import CStorage
 from kvlayer._local_memory import LocalStorage
@@ -23,7 +26,7 @@ try:
 except ImportError:
     PGStorage = None
 
-logger = logging.getLogger('kvlayer')
+logger = logging.getLogger(__name__)
 
 STORAGE_CLIENTS = dict(
     ## these strings deinfe the external API for selecting the kvlayer
@@ -47,69 +50,6 @@ def client(config):
     except Exception, exc:
         logger.critical('config = %r' % config, exc_info=True)
         raise
-
-
-default_config = dict(
-    storage_type = 'local',
-    connection_pool_size = 2,
-    max_consistency_delay = 120,
-    replication_factor = 1,
-    thrift_framed_transport_size_in_mb = 15,
-    )
-
-
-def add_arguments(parser, defaults=None, include_app_name=False, include_namespace=False):
-    '''
-    add command line arguments to an argparse.ArgumentParser instance.
-    This provides sensible defaults and accurate help messages, so
-    that libraries that use kvlayer can provide these flags in their
-    command line interfaces.
-    '''
-    if  defaults is None:
-        defaults = dict()
-
-    if include_app_name:
-        parser.add_argument('--app-name', default=defaults.get('app_name'), 
-                            help='name of app for namespace prefixing')
-    if include_namespace:
-        parser.add_argument('--namespace', default=None, help='namespace for prefixing table names')
-
-    ## standard flags that are unique to kvlayer
-    parser.add_argument('--storage-type', default=default_config.get('storage_type'), 
-                        help='backend type for kvlayer, e.g. "local" or "accumulo"')
-    parser.add_argument('--storage-address', action='append', default=[], dest='storage_addresses',
-                        help='network addresses for kvlayer, can be repeated')
-
-    parser.add_argument('--username', help='username for kvlayer accumulo')
-    parser.add_argument('--password', help='password for kvlayer accumulo')
-
-    parser.add_argument('--connection-pool-size', default=default_config.get('connection_pool_size'),
-                        help='number of connections for kvlayer to open in advance')
-    parser.add_argument('--max-consistency-delay', default=default_config.get('max_consistency_delay'),
-                        help='number of seconds for kvlayer to wait for DB cluster sync')
-    parser.add_argument('--replication-factor', default=default_config.get('replication_factor'),
-                        help='number of copies of the data for kvlayer to require of DB cluster')
-    parser.add_argument('--thrift-framed-transport-size-in-mb', default=default_config.get('thrift_framed_transport_size_in_mb'),
-                        help='must not exceed value set on the server-side of DB cluster.  15MB is hardcoded default in thrift.')
-
-
-def default_yaml():
-    '''
-    return default yaml string for use with yakonfig's !include_func
-    '''
-    return '''
-app_name:  !runtime app_name
-namespace: !runtime namespace
-username: !runtime username
-password: !runtime password
-storage_type: !runtime storage_type
-storage_addresses: !runtime storage_addresses
-connection_pool_size: !runtime connection_pool_size
-max_consistency_delay: !runtime max_consistency_delay
-replication_factor: !runtime replication_factor
-thrift_framed_transport_size_in_mb: !runtime thrift_framed_transport_size_in_mb
-'''
-
 
 def stderr(m, newline='\n'):
     sys.stderr.write(m)
@@ -137,14 +77,8 @@ def main():
     parser.add_argument('action', help='|'.join(allowed_actions))
     parser.add_argument('-y', '--yes', default=False, action='store_true', dest='assume_yes',
                         help='Assume "yes" and require no input for confirmation questions.')
-    add_arguments(parser, include_app_name=True, include_namespace=True)
-    args = parser.parse_args()
-
-    yakonfig.set_runtime_args_object(args)
-
-    fh = StringIO('kvlayer: !include_func kvlayer.default_yaml')
-    config = yakonfig.set_global_config(fh)
-
+    args = yakonfig.parse_args(parser, [yakonfig, kvlayer])
+    config = yakonfig.get_global_config()
     kvlayer_client = client(config['kvlayer'])
 
     if args.action not in allowed_actions:
---
+++ kvlayer/config.py
@@ -0,0 +1,71 @@
+"""Configuration parameters for kvlayer.
+
+These collectively implement the `yakonfig.Configurable` interface.
+
+-----
+
+This software is released under an MIT/X11 open source license.
+
+Copyright 2012-2014 Diffeo, Inc.
+
+"""
+from __future__ import absolute_import
+
+from kvlayer._client import STORAGE_CLIENTS
+from yakonfig import ConfigurationError
+
+config_name = 'kvlayer'
+default_config = dict(
+    storage_type = 'local',
+    connection_pool_size = 2,
+    max_consistency_delay = 120,
+    replication_factor = 1,
+    thrift_framed_transport_size_in_mb = 15,
+    )
+
+def add_arguments(parser):
+    '''Add command line arguments to an argparse.ArgumentParser instance.'''
+    parser.add_argument('--app-name',
+                        help='name of app for namespace prefixing')
+    parser.add_argument('--namespace',
+                        help='namespace for prefixing table names')
+    parser.add_argument('--storage-type',
+                        help='backend type for kvlayer, e.g. "local" or "accumulo"')
+    parser.add_argument('--storage-address', action='append',
+                        dest='storage_addresses', metavar='HOST:PORT',
+                        help='network addresses for kvlayer, can be repeated')
+    parser.add_argument('--username', help='username for kvlayer accumulo')
+    parser.add_argument('--password', help='password for kvlayer accumulo')
+    parser.add_argument('--connection-pool-size', metavar='N',
+                        help='number of connections for kvlayer to open in advance')
+    parser.add_argument('--max-consistency-delay', metavar='SECONDS',
+                        help='number of seconds for kvlayer to wait for DB cluster sync')
+    parser.add_argument('--replication-factor', metavar='N',
+                        help='number of copies of the data for kvlayer to require of DB cluster')
+    parser.add_argument('--thrift-framed-transport-size-in-mb', metavar='MB',
+                        help='must not exceed value set on the server-side of DB cluster.  15MB is hardcoded default in thrift.')
+
+runtime_keys = dict(
+    app_name = 'app_name',
+    namespace = 'namespace',
+    username = 'username',
+    password = 'password',
+    storage_type = 'storage_type',
+    storage_addresses = 'storage_addresses',
+    connection_pool_size = 'connection_pool_size',
+    max_consistency_delay = 'max_consistency_delay',
+    replication_factor = 'replication_factor',
+    thrift_framed_transport_size_in_mb = 'thrift_framed_transport_size_in_mb'
+)
+
+def check_config(config, name):
+    if 'storage_type' not in config:
+        raise ConfigurationError('{} must have a storage_type'
+                                 .format(name))
+    if config['storage_type'] not in STORAGE_CLIENTS:
+        raise ConfigurationError('invalid {} storage_type {}'
+                                 .format(name, config['storage_type']))
+    if 'namespace' not in config:
+        raise ConfigurationError('{} requires a namespace'.format(name))
+    if 'app_name' not in config:
+        raise ConfigurationError('{} requires an app_name'.format(name))
